0a622bfd40080e7f1a03873f549ae0e2427df851
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-09-13 18:26:43 +0200

    Update setup-ubuntu-2204.sh

--- setup-ubuntu-2204.sh
+++ setup-ubuntu-2204.sh
@@ -14,3 +14,10 @@ chmod +x /usr/bin/terragrunt
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 az aks install-cli
 
+# Docker
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-cache policy docker-ce
+apt-get install -y docker-ce
