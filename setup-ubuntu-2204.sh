c396a8014850d8027028f9f6450f86384597184a
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-09-14 15:30:36 +0200

    Update setup-ubuntu-2204.sh

--- setup-ubuntu-2204.sh
+++ setup-ubuntu-2204.sh
@@ -21,3 +21,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-cache policy docker-ce
 apt-get install -y docker-ce
+sudo usermod -a -G docker azuser
